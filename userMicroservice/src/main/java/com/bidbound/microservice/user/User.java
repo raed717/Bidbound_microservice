50df337a3627a41e4cbb3755d97fa5a4b3dd7ec1
RaedGuembri <raed.guembri@esprit.tn>
2023-10-18 23:43:56 +0100

    crud user

---
+++ userMicroservice/src/main/java/com/bidbound/microservice/user/User.java
@@ -0,0 +1,58 @@
+package com.bidbound.microservice.user;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String nom;
+    private String prenom;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
---
+++ userMicroservice/src/main/java/com/bidbound/microservice/user/userServiceImpl.java
@@ -0,0 +1,54 @@
+package com.bidbound.microservice.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class userServiceImpl implements IuserService {
+
+    @Autowired
+    private userRepository userRepository;
+
+    @Override
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findUserById(Long id) {
+        Optional<User> user = userRepository.findById(id);
+        return user.orElse(null);
+    }
+
+    @Override
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public User updateUser(Long id, User userDetails) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User existingUser = optionalUser.get();
+
+            // Update the user details
+            existingUser.setNom(userDetails.getNom());
+            existingUser.setPrenom(userDetails.getPrenom());
+            existingUser.setEmail(userDetails.getEmail());
+            existingUser.setPassword(userDetails.getPassword());
+
+            return userRepository.save(existingUser);
+        } else {
+            return null; // User with the specified ID not found
+        }
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+
+    }
+}
+
