4777ada130190bb5cefc32b0e2b11f866480908c
firasyazid <98762038+firasyazid@users.noreply.github.com>
2023-10-19 22:54:39 +0100

    eureka + ms_product

--- ProductMicroservice/Product/src/main/java/com/microservice/product/product/ProductApplication.java
+++ ProductMicroservice/Product/src/main/java/com/microservice/product/product/ProductApplication.java
@@ -2,9 +2,12 @@ package com.microservice.product.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class ProductApplication {
+@EnableDiscoveryClient
+
+    public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
