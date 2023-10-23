0c94c25afe1e6e95335817808a596d6c4e4b816f
mensia <hana.mensia@esprit.tn>
2023-10-23 14:25:05 +0100

    modifi controller & add docker file

--- PariMicroService/src/main/java/com/bidbound/microservice/parimicroservice/controller/PariController.java
+++ PariMicroService/src/main/java/com/bidbound/microservice/parimicroservice/controller/PariController.java
@@ -2,36 +2,45 @@ package com.bidbound.microservice.parimicroservice.controller;
 
 import com.bidbound.microservice.parimicroservice.entite.Pari;
 import com.bidbound.microservice.parimicroservice.service.IPariService;
-import lombok.AllArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@AllArgsConstructor
+
 @CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/paris")
 public class PariController {
+    public PariController(IPariService pariService) {
+        this.pariService = pariService;
+    }
 
     private IPariService pariService ;
     @GetMapping("/All")
-   public  List<Pari> findAllParis(){
-        return this.pariService.findAllParis();
+   public  ResponseEntity<List<Pari>> findAllParis(){
+        List<Pari> ListPari =  this.pariService.findAllParis() ;
+        return new ResponseEntity<>(ListPari , HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    public Pari findPariById(@PathVariable Long id){
-        return this.pariService.findPariById(id) ;
+    public ResponseEntity<Pari> findPariById(@PathVariable Long id){
+        return new ResponseEntity<>(this.pariService.findPariById(id)  , HttpStatus.OK);
     }
     @PostMapping("/Add")
-    public Pari createPari(@RequestBody Pari pari){
-        return this.pariService.createPari(pari) ;
+    public ResponseEntity<Pari>  createPari(@RequestBody Pari pari){
+        return new ResponseEntity<> ( this.pariService.createPari(pari) , HttpStatus.CREATED);
     }
     @PutMapping("/Put")
-    public Pari updatePari( @RequestBody Pari Newpari){
-        return this.pariService.updatePari(Newpari) ;
+    public ResponseEntity<Pari>  updatePari( @RequestBody Pari Newpari){
+        if ( this.pariService.updatePari(Newpari)  != null )
+        return new ResponseEntity<> ( this.pariService.updatePari(Newpari)  , HttpStatus.ACCEPTED) ;
+        else return  new ResponseEntity<> (null , HttpStatus.NOT_FOUND) ;
      }
      @DeleteMapping("/Delete")
-    public void deletePari(@PathVariable Long id){
-         this.pariService.deletePari(id);
+    public ResponseEntity<Void> deletePari(@PathVariable Long id){
+           this.pariService.deletePari(id)  ;
+         return new ResponseEntity<> (HttpStatus.OK ) ;
      }
 }
